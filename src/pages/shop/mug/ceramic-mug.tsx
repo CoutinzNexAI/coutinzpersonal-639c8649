bc8d5fbbeea0e3c216cae25efa90923e3d43d6e5
CoutinzNexAI <diogonexai@gmail.com>
2025-06-27 17:28:39 +0100

    atira

--- src/pages/shop/mug/ceramic-mug.tsx
+++ src/pages/shop/mug/ceramic-mug.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Link from 'next/link';
