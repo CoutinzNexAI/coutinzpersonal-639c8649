611a527cd09c4419c49288608c6359911ecdba80
CoutinzNexAI <diogonexai@gmail.com>
2025-07-15 12:06:15 +0100

    fb certo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -114,7 +114,6 @@ function MyApp({ Component, pageProps }: AppProps) {
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '${fpixel.FB_PIXEL_ID}');
-            fbq('track', 'PageView');
           `,
         }}
       />
